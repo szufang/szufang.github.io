039bad9246a6c3677756b61ba1f8cb9a6a7bef4f
Szu-Fang <szufang19@gmail.com>
2020-04-10 20:22:27 -0400

    php test

--- php/sendmail.php
+++ php/sendmail.php
@@ -16,7 +16,7 @@ if( isset($name) && isset($email) ) {
 	}
 
 	// Email will be send
-	$to = "mailexample62@gmail.com"; // Change with your email address
+	$to = "szufang19@gmail.com"; // Change with your email address
 	$sub = "$subject from CV"; // You can define email subject
 	// HTML Elements for Email Body
 	$body = <<<EOD
